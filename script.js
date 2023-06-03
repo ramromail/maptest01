01f50290c34fba61f8f9177cd2df81130f3aac3e
beginner <6681387+ramromail@users.noreply.github.com>
2023-06-03 13:35:19 +0000

    Update script.js
    add watch position

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Initialize and add the map
-
-var gMap, infoWindow, marker;
-
+var gMap, infoWindow, marker, watchPos;
 initMap();
 
 function initMap() {
     const myLatLng = { lat: 0, lng: 0 };
+
     gMap = new google.maps.Map(document.getElementById("gMap"), {
         zoom: 2,
         center: myLatLng,
@@ -24,25 +23,49 @@ let gpsBtn = document.querySelector('input#locateMeGPS[type=checkbox]');
 gpsBtn.addEventListener("change", function () {
     if (this.checked) {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    const pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
+            // navigator.geolocation.getCurrentPosition(
+            //     (position) => {
+            //         const pos = {
+            //             lat: position.coords.latitude,
+            //             lng: position.coords.longitude,
+            //         };
+
+            //         marker.setPosition(pos);
+            //         gMap.panTo(pos);
+            //         gMap.setZoom(16);
+            //     },
+            //     () => {
+            //         handleLocationError(true, infoWindow, gMap.getCenter());
+            //     });
+
+            watchPos = navigator.geolocation.watchPosition(
+                function (pos) {
+                    // successfunction
+                    const crd = {
+                        lat: pos.coords.latitude,
+                        lng: pos.coords.longitude,
                     };
-                    marker.setPosition(pos);
-                    gMap.panTo(pos);
+                    console.log(pos);
+                    gMap.panTo(crd);
+                    marker.setPosition(crd);
                     gMap.setZoom(16);
-                },
-                () => {
-                    handleLocationError(true, infoWindow, gMap.getCenter());
-                });
+                }, function (err) {
+                    // errors occured
+                    console.error('Error occured, ', err);
+                }, {
+                // options
+                enableHighAccuracy: true,
+                timeout: 120000,
+                maximumAge: 10000
+            });
+
         } else {
             // Browser doesn't support Geolocation
             handleLocationError(false, infoWindow, gMap.getCenter());
         }
     } else {
         console.log("Checkbox is not checked..");
+        navigator.geolocation.clearWatch(watchPos);
     }
 });
 
@@ -55,6 +78,3 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     );
     infoWindow.open(gMap);
 }
-
-
-
